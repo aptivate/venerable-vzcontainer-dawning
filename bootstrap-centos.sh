9a778e1005420258ee5e32b8653ba4ac938f12b7
adrians <adrians@679a26be-e637-0410-a801-b7d8065a4221>
2013-10-28 16:29:02 +0000

    Getting svn and github branches back in sync
    git-svn-id: https://svn.aptivate.org/svn/infrastructure/servers/fen-vzX/vz-bootstrap-scripts@10759 679a26be-e637-0410-a801-b7d8065a4221

--- bootstrap-centos.sh
+++ bootstrap-centos.sh
@@ -15,7 +15,7 @@ HOSTNAME="$2"
 
 if ! validCTID "$CTID" ; then
 	echo "Invalid CITD \"$CTID\""
-	exit 1 
+	exit 1
 fi
 
 if [ -d $VZROOT/$CTID ] ; then
@@ -32,12 +32,12 @@ fi
 
 # http://download.openvz.org/template/precreated/centos-5-x86-devel.tar.gz
 vzctl create $CTID --ostemplate centos-5-standard_5.6-1_i386 &&
-vzctl set $CTID --applyconfig $VZCONFIG --save && 
-vzctl set $CTID --ipadd 10.0.156.$CTID --save && 
-vzctl set $CTID --nameserver "10.0.156.4 10.0.156.8" --save && 
-vzctl set $CTID --hostname $HOSTNAME.fen.aptivate.org --save && 
-vzctl set $CTID --diskspace 6G:8G --save && 
-vzctl set $CTID --privvmpages 512M:1G --save && 
+vzctl set $CTID --applyconfig $VZCONFIG --save &&
+vzctl set $CTID --ipadd 10.0.156.$CTID --save &&
+vzctl set $CTID --nameserver "10.0.156.4 10.0.156.8" --save &&
+vzctl set $CTID --hostname $HOSTNAME.fen.aptivate.org --save &&
+vzctl set $CTID --diskspace 6G:8G --save &&
+vzctl set $CTID --privvmpages 512M:1G --save &&
 vzctl set $CTID --name $HOSTNAME --save &&
 vzctl start $CTID --wait
 
@@ -54,4 +54,3 @@ exit
 ## post install
 # need to get it configured quick - really cfengine/puppet job.
 # follow the instructions at https://wiki.aptivate.org/Wiki.jsp?page=NetworkInfrastructure.Puppet
-
